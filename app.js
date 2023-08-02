37d78c3f798e4f5220d2c38b7058fc686e1a015e
kehinde yusuf <yusufkehindemubaraq@gmail.com>
2023-08-02 21:12:09 +0100

    I built a slider for a better user experience

--- app.js
+++ app.js
@@ -1,17 +1,116 @@
-let cardOneDetails = document.querySelector('.cardOneDetails')
-let firstCard = document.querySelector('.firstCard');
-let removeBtn = document.querySelector('.removeBtn')
-let messageOne = document.querySelector('.messageOne')
-const buyBtn = document.querySelector('.buyBtn')
+const carDetails = [
+  {
+    id: 1,
+    img: "./benz.jpg",
+    title: "Rolls Royce 1960",
+    interiorColor: "Gray",
+    exteriorColor: "Black",
+    type: "Manual",
+  },
+  {
+    id: 2,
+    img: "./none.jpg",
+    title: "BMW 2020",
+    interiorColor: "Gray",
+    exteriorColor: "Black",
+    type: "automatic",
+  },
+  {
+    id: 3,
+    img: "./none.jpg",
+    title: "Rolls Royce 1960",
+    interiorColor: "Gray",
+    exteriorColor: "Black",
+    type: "Manual",
+  },
+  {
+    id: 4,
+    img: "./none.jpg",
+    title: "Rolls Royce 1960",
+    interiorColor: "Gray",
+    exteriorColor: "Black",
+    type: "Manual",
+  },
+  {
+    id: 5,
+    img: "./none.jpg",
+    title: "Rolls Royce 1960",
+    interiorColor: "Gray",
+    exteriorColor: "Black",
+    type: "Manual",
+  },
+  {
+    id: 6,
+    img: "./none.jpg",
+    title: "Rolls Royce 1960",
+    interiorColor: "Gray",
+    exteriorColor: "Black",
+    type: "Manual",
+  },
+  {
+    id: 7,
+    img: "./none.jpg",
+    title: "Rolls Royce 1960",
+    interiorColor: "Gray",
+    exteriorColor: "Black",
+    type: "Manual",
+  },
+  {
+    id: 8,
+    img: "./none.jpg",
+    title: "Rolls Royce 1960",
+    interiorColor: "Gray",
+    exteriorColor: "Black",
+    type: "Manual",
+  },
+ 
+];
 
-firstCard.addEventListener('click', ()=>{
-    cardOneDetails.style.display = 'block'
-})
+// Rendering The Car Detail Dynamically To The DOM
+function carDetailsContainer() {
+  const sliderContainer = document.querySelector('.slider-container');
+  
+  // Iterating Through The Car Details
 
-removeBtn.addEventListener('click',()=>{
-    cardOneDetails.style.display = 'none'
-})
+carDetails.map(carDetail => {
+  sliderContainer.innerHTML += `<div class='slider-content'>
+                                <img src=${carDetail.img} alt=${carDetail.title}/>
+                            </div>`
+});
 
-buyBtn.addEventListener('click',()=>{
-    messageOne.style.display = 'block'
-})
+}
+carDetailsContainer()
+
+let currentIndex = 0;
+const images = document.querySelectorAll(".slider-content img");
+const totalImages = images.length;
+
+// Display Image on The Browser
+
+function showImage(index) {
+  images.forEach((img, i) => {
+    if (i === index) {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  });
+}
+
+// Show Next Image 
+function nextImage() {
+  currentIndex = (currentIndex + 1) % totalImages;
+  showImage(currentIndex);
+}
+
+// Show Previous Image
+function prevImage() {
+  currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+  showImage(currentIndex);
+}
+
+const prevButton = document.querySelector(".bxs-chevron-left");
+const nextButton = document.querySelector(".bxs-chevron-right");
+
+nextButton.addEventListener("click", nextImage);
+prevButton.addEventListener("click", prevImage);
